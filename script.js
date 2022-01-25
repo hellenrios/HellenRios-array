25c25e498e2cb0cf08d6f320ad4a75d27ee093a4
Hellen Rios <hellenrioss@gmail.com>
2022-01-25 19:34:40 -0300

    adicionando desafio 05

---
+++ script.js
@@ -0,0 +1,23 @@
+//1
+
+let countries = ["Brazil", "Spain", "Angola", "China", "Canada"]
+console.log(countries)
+
+//2
+
+countries.unshift("Morocco")
+
+//3
+
+countries.pop()
+
+//4
+
+countries.push("Albania", "Germany")
+
+//5 
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
+
